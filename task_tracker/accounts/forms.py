94e94dfdc125a8d355bdbe52145d6631d4ebe2ed
polina_storozh <58592174+voird@users.noreply.github.com>
2025-02-13 19:25:11 +0300

    Update forms.py

--- task_tracker/accounts/forms.py
+++ task_tracker/accounts/forms.py
@@ -57,3 +57,17 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['avatar', 'bio']
+
+
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'start_date', 'end_date', 'status']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
