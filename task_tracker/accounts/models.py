aa22a73de98bb9ee3095e0debad69397b6717598
polina_storozh <58592174+voird@users.noreply.github.com>
2025-02-13 19:25:39 +0300

    Update models.py

--- task_tracker/accounts/models.py
+++ task_tracker/accounts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.forms import ValidationError
 
 
 class Profile(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
@@ -21,3 +22,24 @@ class Profile(models.Model):
             new_img = (100, 100)
             img.thumbnail(new_img)
             img.save(self.avatar.path)
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    assigned_to = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def clean(self):
+        if self.start_date and self.end_date and self.start_date > self.end_date:
+            raise ValidationError('Дата начала не может быть позже даты окончания!')
+
+    def __str__(self):
+        return self.title
